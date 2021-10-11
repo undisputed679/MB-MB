0941b69d74e277610cab6949d37b9486ee5d93cb
undisputed679 <mayankbhushan679@gmail.com>
2021-10-11 12:50:09 +0530

    made different cards and photos added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Homepage } from './pages/homepage/HomepageComponent';
+
+function App() {
+  return (
+    <div className="App">
+    <Homepage/>
+    </div>
+  );
+}
+
+export default App;
